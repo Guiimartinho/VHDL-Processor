e2b82c1de575271a25b37d1597f6282a5442fb2f
Angelin01 <angeloborsoiross@hotmail.com>
2017-06-03 17:09:40 -0300

    Melhorados comentários da control unit

--- Aula 7/controlunit.vhd
+++ Aula 7/controlunit.vhd
@@ -32,12 +32,12 @@ architecture a_controlunit of controlunit is
 begin
 	opcode <= instruction(13 downto 10);
 	
-	branch_en <= '1' when opcode="1101" else '0';
-    jmp_en <= '1' when opcode="1000" or 
-                        (opcode="1001" and negativeFlag = '1') or 
-                        (opcode="1010" and zeroFlag = '1') or 
-                        (opcode="1011" and zeroFlag = '0') 
-                        else '0';
+	branch_en <= '1' when opcode="1101" else '0'; -- Branch
+    jmp_en <= '1' when opcode="1000" or  -- JMP Incondicional
+                        (opcode="1001" and negativeFlag = '1') or -- JN (Jump if Negative)
+                        (opcode="1010" and zeroFlag = '1') or -- JEQ (Jump if Equal)
+                        (opcode="1011" and zeroFlag = '0') -- JNE (Jump if Not Equal)
+                        else '0'; -- Não é jump
 	
 	alu_op <= "00" when opcode="0000" or opcode="0001" else -- ADD
 			  "01" when opcode="0010" or opcode="0011" or opcode="1100" else -- SUB ou CMP
@@ -50,17 +50,17 @@ begin
 	reg_write_source <= "00" when opcode="0000" or opcode="0001" or opcode="0010" or opcode="0011" else -- ADD ou SUB (pega o dado da ULA)
 						"01" when opcode="0100" else -- MOV Rs, Rd (pega o dado do banco de registradores)
 						"10" when opcode="0101" else -- MOV #imm, Rd (pega o dado da instrução)
-						"11"; -- Ainda não implementadas instruções de memória
+						"11"; -- MOV @Rs, Rd ou MOV Rs, @Rd (pega ou coloca o dado na memória do registrador com @)
 	
 	reg_write <= '1' when state = "10" and (opcode="0000" or opcode="0001" or opcode="0010" or opcode="0011" or opcode="0100" or opcode="0101" or opcode="0110") else -- Operações que escrevem no banco de registradores
 				 '0'; -- Qualquer outra operação
 	
-	flags_write <= '1' when state = "10" and (opcode="0000" or opcode="0001" or opcode="0010" or opcode="0011" or opcode="1100") else
+	flags_write <= '1' when state = "10" and (opcode="0000" or opcode="0001" or opcode="0010" or opcode="0011" or opcode="1100") else -- Apenas essas operações alteram as flags
 				   '0';
-	
+	-- Seletor de endereço da RAM: se o Rs ou Rd, dependendo do opcode
 	ram_addr_sel <= '1' when opcode="0110" else -- Rs
 					'0'; -- Rd
-	
+	-- Escrever ou não na RAM
 	ram_write <= '1' when state = "10" and opcode="0111" else
 				 '0';
 	
